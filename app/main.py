e20cb651cd987b25f2d541c224751628c92ce21d
idalz <idalez@outlook.com>
2025-04-29 23:27:20 +0300

    Create frontend

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints.chat import router as chat_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi import Request
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 app.include_router(chat_router, prefix="/api/v1")
+ 
